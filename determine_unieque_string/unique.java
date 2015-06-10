d6408c6438db8f753403459fb28cd63bb90d3b62
Xin (Chris) Zou <x9zou@uwaterloo.ca>
2015-06-09 23:58:42 -0400

    Update unique.java

--- determine_unieque_string/unique.java
+++ determine_unieque_string/unique.java
@@ -1,9 +1,18 @@
 public static boolean isUniqueChars2(String str) {
+  
+  // create an array of boolean values
   boolean[] char_set = new boolean[256];
+  
+  // iterate through the string
    for (int i = 0; i < str.length(); i++) {
+    
+    // check whether i-th character is in this string, store the integer (1 or 0) in val.  (this is O(1))
     int val = str.charAt(i);
+    
     if (char_set[val]) return false;
       char_set[val] = true;
       }
+      
+    // jump out and the string contains unique characters
     return true;
  }
